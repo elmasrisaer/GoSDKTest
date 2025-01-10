f92e06ca4566b4bbfabb81ab1a92f979da03f901
elmasrisaer <139119606+elmasrisaer@users.noreply.github.com>
2025-01-10 12:10:24 +0000

    liblab SDK update for version v1.0.2

--- pkg/esim/request_params.go
+++ pkg/esim/request_params.go
@@ -1,7 +1,7 @@
 package esim
 
 type GetEsimRequestParams struct {
-	Iccid *string `maxLength:"22" minLength:"18" queryParam:"iccid" required:"true"`
+	Iccid *string `explode:"true" serializationStyle:"form" maxLength:"22" minLength:"18" queryParam:"iccid" required:"true"`
 }
 
 func (params *GetEsimRequestParams) SetIccid(iccid string) {
